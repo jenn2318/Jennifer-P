0d67b66bcdcdce4e426bd38546268b5b410d9938
Jennifer Ponder <jennp@Jennifers-MacBook-Pro.local>
2018-06-10 20:26:27 -0400

    edited index file and added routing directory

---
+++ public/routing/html-routes.js
@@ -0,0 +1,20 @@
+var path = require("path");
+
+//GET Requests...this code will handle when a user visits the home, projects or contacts page
+
+module.exports = function(app) {
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+    app.get("/survey", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/projects.html"));
+
+    });
+
+
+    app.get(function(req, res) {
+        res.sendFile(path.join(__dirname, "/..public/contact.html"));
+
+    });
+
+};
